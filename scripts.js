b81f624459df4c58fa182469a7b3b5dc9c3db760
Daniele Ricci <daniele@casaricci.it>
2025-12-31 12:10:56 +0100

    Very basic and ugly UI

--- scripts.js
+++ scripts.js
@@ -9,6 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
     [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
     $.getJSON("http://localhost:8787/latest?limit=1", function (data) {
-        $("#currentWeatherData").text(JSON.stringify(data[0], null, 2));
+        let latest = data[0];
+        $("#currentWeatherData").text(JSON.stringify(latest, null, 2));
+
+        let timestamp = new Date(latest['timestamp']);
+
+        $('#dataTimestamp').text(timestamp.toLocaleString());
+        $('#dataTemperature').text(latest['temperature']);
+        $('#dataHumidity').text(latest['humidity']);
+        $('#dataDewPoint').text(latest['dew_point']);
+        $('#dataPressure').text(latest['pressure']);
+        $('#dataWindSpeed').text(latest['wind_speed']);
+        $('#dataWindDirection').text(latest['wind_direction']);
     });
 });
